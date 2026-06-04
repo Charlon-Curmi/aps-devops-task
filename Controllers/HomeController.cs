8d7989b5ae0ab0b9d30381c494be3492532b11ef
Charlon Curmi <charlon.curmi678@gmail.com>
2026-06-04 17:38:00 +0200

    Add new Environments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,14 +7,20 @@ namespace APSBankDemo.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(
+        ILogger<HomeController> logger,
+        IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
     {
+        ViewBag.EnvironmentName = _configuration["EnvironmentName"];
+
         return View();
     }
 
